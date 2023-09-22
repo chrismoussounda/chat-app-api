4c5c32e1d79eae4c698216ee7b618992fe8648df
Christoffert Moussounda <christmoussounda@gmail.com>
2023-09-22 18:46:56 +0100

    Update socket

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -3,13 +3,22 @@ import {
   SubscribeMessage,
   MessageBody,
   WebSocketServer,
+  OnGatewayConnection,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { UsePipes, ValidationPipe } from '@nestjs/common';
 
 @UsePipes(new ValidationPipe({ whitelist: true }))
-@WebSocketGateway(Number(process.env.PORT) || 8000, { cors: true })
-export class SocketGateway {
+@WebSocketGateway({ cors: true })
+export class SocketGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Socket;
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Client connected from host: ${client.conn.remoteAddress}`);
+    console.log(`Client connected from host: ${client.handshake.address}`);
+    console.log(
+      `Connection established on port: ${client.handshake.headers.host}`,
+    );
+  }
 }
